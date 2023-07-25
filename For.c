16057fbd3e39f8e4ce8f8092ff39d1af57d55f5f
Marco <maphy1707@gmail.com>
2023-07-24 22:05:46 -0600

    Primer For

--- For.c
+++ For.c
@@ -4,11 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	/**Este programa muestra las tablas de multiplicar del 0 hasta la del 10**/
+	
+	/**Estas variables de tipo entero inicializan contadores**/
 	int contador;
 	int contadorDos;
 	
 	for(contador = 0; contador <= 10; contador++){
-		//printf("\n%i", contador);
 		printf("\n");
 		printf("\nTabla del numero %i", contador);
 		for(contadorDos = 0; contadorDos<=10; contadorDos++){
